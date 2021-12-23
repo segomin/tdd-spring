44dc0b71a9af58c2b031f9b5244045680d9e39fa
sangho <segoviamin@gmail.com>
2021-12-23 11:04:08 +0900

    # Red: get about -> message

--- src/test/kotlin/me/segomin/school/rest/SchoolControllerTests.kt
+++ src/test/kotlin/me/segomin/school/rest/SchoolControllerTests.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -28,6 +29,8 @@ class SchoolControllerTests {
 
     @Test
     internal fun aboutReturnsSchoolInfo() {
-        mockMvc.perform(get("/about")).andExpect(status().isOk)
+        mockMvc.perform(get("/about"))
+            .andExpect(status().isOk)
+            .andExpect(content().string("Welcome to TDD School"))
     }
 }
