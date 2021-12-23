b8723b4bec2086a9bc9c38a16af07ea2794306db
sangho <segoviamin@gmail.com>
2021-12-23 13:19:41 +0900

    # Green: get about with security

---
+++ src/main/kotlin/me/segomin/school/config/SecurityConfig.kt
@@ -0,0 +1,20 @@
+package me.segomin.school.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.httpBasic(Customizer.withDefaults())
+            .authorizeRequests{ authz ->
+                authz.anyRequest().permitAll()
+            }
+        return http.build()
+    }
+}
